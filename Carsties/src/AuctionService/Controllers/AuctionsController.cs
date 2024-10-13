001ecdbdf235c9f606372a9b585b37a3f6fb1f0c
edemirkaya <ertugrulldemirkaya@gmail.com>
2024-10-14 00:06:57 +0300

    RabbitMq basicly entegrated for Auction CRUD

--- Carsties/src/AuctionService/Controllers/AuctionsController.cs
+++ Carsties/src/AuctionService/Controllers/AuctionsController.cs
@@ -84,6 +84,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (result) return Ok();
@@ -99,6 +101,8 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         _context.Auctions.Remove(auction);
+
+        await _publishEndpoint.Publish<AuctionDeleted>(new {Id = auction.Id.ToString()});
 
         var result = await _context.SaveChangesAsync() > 0;
 
